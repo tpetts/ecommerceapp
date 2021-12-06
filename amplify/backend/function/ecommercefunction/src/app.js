fde71e721285f7f84807df1e9ad00e41ca7e4cc7
Tia Petts <tpetts@madisoncollege.edu>
2021-12-06 13:01:50 -0600

    Adding in ecommerce function

--- amplify/backend/function/ecommercefunction/src/app.js
+++ amplify/backend/function/ecommercefunction/src/app.js
@@ -20,8 +20,66 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 
+// Defining the constants! 
 const AWS = require('aws-sdk')
 const { v4: uuid } = require('uuid')
+
+/* Cognito SDK */
+const cognito = new
+AWS.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18'
+})
+
+/* Cognito User Pool ID
+*  This User Pool ID variable will be given to you by the CLI output after
+   adding the category
+*  This will also be available in the file itself, commented out at the top
+*/
+var userpoolId = process.env.AUTH_ECOMMERCEAPPB5769943_USERPOOLID
+
+// DynamoDB configuration
+const region = process.env.REGION
+const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME
+const docClient = new AWS.DynamoDB.DocumentClient({region})
+
+
+// amplify/backend/function/ecommercefunction/src/app.js
+const getGroupsForUser = async (event) => {
+  let userSub =
+    event
+      .requestContext
+      .identity
+      .cognitoAuthenticationProvider
+      .split(':CognitoSignIn:')[1]
+  let userParams = {
+    UserPoolId: userpoolId,
+    Filter: `sub = "${userSub}"`,
+  }
+  let userData = await cognito.listUsers(userParams).promise()
+  const user = userData.Users[0]
+  var groupParams = {
+    UserPoolId: userpoolId,
+    Username: user.Username
+  }
+  const groupData = await cognito.adminListGroupsForUser(groupParams).promise()
+  return groupData
+}
+
+const canPerformAction = async (event, group) => {
+  return new Promise(async (resolve, reject) => {
+    if (!event.requestContext.identity.cognitoAuthenticationProvider) {
+      return reject()
+    }
+    const groupData = await getGroupsForUser(event)
+    const groupsForUser = groupData.Groups.map(group => group.GroupName)
+    if (groupsForUser.includes(group)) {
+      resolve()
+    } else {
+      reject('user not in group, cannot perform action..')
+    }
+  })
+}
+
 
 // declare a new express app
 var app = express()
@@ -42,6 +100,9 @@ app.use(function(req, res, next) {
 
 app.get('/products', function(req, res) {
   // Add your code here
+  
+
+
   res.json({success: 'get call succeed!', url: req.url});
 });
 
